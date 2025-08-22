ce25e2282f947d3003f901be29a45485686125ab
sandeep <145739064+sandyjyp@users.noreply.github.com>
2025-08-22 15:13:48 +0530

    Update clone.groovy

--- clone.groovy
+++ clone.groovy
@@ -1,4 +1,5 @@
 def call(string url, string branch){
   echo"this is cloning the code"
-               git url: "https://github.com/sandyjyp/jenkins-shared-libraries.git", branch:"main"
-               echo"code cloning successful"
+  git url: "https://github.com/sandyjyp/jenkins-shared-libraries.git", branch:"main"
+  echo"code cloning successful"
+}
